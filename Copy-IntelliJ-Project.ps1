0269880bb8bacfd2b3f6415c448e8a9ab8b191b8
Joe-Demp <dempseyjoseph96@yahoo.com>
2020-05-27 22:53:47 +0100

    Adding the java in a different location.

--- Copy-IntelliJ-Project.ps1
+++ Copy-IntelliJ-Project.ps1
@@ -1,5 +1,5 @@
 # A script to copy files from an IntelliJ project on my computer to this git repo
-Copy-Item -Path "C:\Users\demps\IntelliJProjects\Experiments\src\leetcode" -Destination "C:\Users\demps\Documents\Code\leetcode\IntelliJ" -Recurse
+Robocopy.exe "C:\Users\demps\IntelliJProjects\Experiments\src\leetcode" "C:\Users\demps\Documents\Code\leetcode\IntelliJ" /E /XO
 
 # todo include an extra script to do an automated commit of IntelliJ files (only, and if required)
 # todo Logging?
---
+++ IntelliJ/Dummy/Dummy.java
@@ -0,0 +1,7 @@
+package leetcode.Dummy;
+
+class Dummy {
+    void sayHello() {
+        System.out.println("Hello, I'm a Dummy!");
+    }
+}
